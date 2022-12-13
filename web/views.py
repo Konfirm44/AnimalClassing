60c5f35ef9707576a33a553cdf5b933b38ce2166
MultiPrice <igobud01@gmail.com>
2022-12-13 17:42:22 +0100

    view images from database

--- web/views.py
+++ web/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
+from .models import Image
 # Create your views here.
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the web index.")
+    # Pobranie pozycji z bazy danych
+    images = Image.objects.all()
+    # Stworzenie słownika przechowującego elementy
+    # bazy danych pod zmienną news
+    context = {'images': images}
+    # Przełªanie wyrenderowanej strony wraz z dodanymi
+    # elementami z bazy danych
+    # elementy ze słownika context wykorzytywane
+    # są w pliku news/index.html
+    return render(request, 'index.html', context)
