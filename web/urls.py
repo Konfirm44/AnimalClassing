ac2d22b1a91f6550986fc61e7c9a34495f7fb395
MultiPrice <igobud01@gmail.com>
2022-12-13 18:17:54 +0100

    Upload nwe images

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add', views.add, name='add'),
+    path('success', views.success, name='success'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,18 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Image
+from .forms import ImageForm
+from django.utils import timezone
 # Create your views here.
 
 from django.http import HttpResponse
 
 
 def index(request):
-    # Pobranie pozycji z bazy danych
     images = Image.objects.all()
-    # Stworzenie słownika przechowującego elementy
-    # bazy danych pod zmienną news
     context = {'images': images}
-    # Przełªanie wyrenderowanej strony wraz z dodanymi
-    # elementami z bazy danych
-    # elementy ze słownika context wykorzytywane
-    # są w pliku news/index.html
     return render(request, 'index.html', context)
+
+    
+def add(request): 
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+ 
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.dateAdded = timezone.now()
+            form.save()
+            return redirect('success')
+    else:
+        form = ImageForm()
+    return render(request, 'add.html', {'form': form})
+ 
+ 
+def success(request):
+    return HttpResponse('successfully uploaded')
