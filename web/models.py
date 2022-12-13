7fa04b680e12b2212683841a003d9feb2b8d77c3
Tomasz Sitek <tomsitek@gmail.com>
2022-12-13 16:45:40 +0100

    add image model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    title = models.CharField(max_length=50)
+    dateAdded = models.DateTimeField('date added')
+    image = models.ImageField(upload_to='images')
+
