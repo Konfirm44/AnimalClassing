56a68297d27cd588794c3fb55c50a395418ca2c7
Tomasz Sitek <tomsitek@gmail.com>
2022-12-13 17:54:04 +0100

    add dateClassified to image model

--- web/models.py
+++ web/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Image(models.Model):
     title = models.CharField(max_length=100)
-    dateAdded = models.DateTimeField('date added')
+    dateAdded = models.DateTimeField('added on')
     image = models.ImageField(upload_to='images')
     classification = models.CharField(max_length=50, default='waiting for classification')
+    dateClassified = models.DateTimeField('classified on', default=None, null=True, blank=True)
 
