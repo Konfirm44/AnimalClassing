56652a23156285894b3ad354707586709a147016
Tomasz Sitek <tomsitek@gmail.com>
2022-12-13 17:49:12 +0100

    add classification field to image model

--- web/models.py
+++ web/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Image(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     dateAdded = models.DateTimeField('date added')
     image = models.ImageField(upload_to='images')
+    classification = models.CharField(max_length=50, default='waiting for classification')
 
